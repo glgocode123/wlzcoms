e7ba8b9672959c4be6e1a0b2825c4e95c2dbfa80
glgocode123 <624601538@qq.com>
2020-08-14 18:12:07 +0800

    Update shopcart.js

--- js/shopcart.js
+++ js/shopcart.js
@@ -112,7 +112,7 @@ $(function () {
 		//读取COOKIE中的集合
 		this.Read = function () {
 			var source = $.cookie(this.cookieName);
-			alert(source);
+//			alert(source);
 			var cart = new Cart();
 			if (source === null || source === "") {
 				return cart;
@@ -151,18 +151,18 @@ $(function () {
 		var pro_list = xc.Read();
 
 		var abs = pro_list.Items;
-		alert(abs);
+//		alert(abs);
 
 
-		var username = $.cookie("username");
-		alert(username);
+//		var username = $.cookie("username");
+//		alert(username);
 
 		// alert(abs[0]["Id"]);
 
 		var ul = '';
 		for (var i = 0; i < abs.length; i++) {
-			alert("123");
-			ul += '<ul class="cart_list_td clearfix" v-for="(sku,index) in cart" id="porcdesc" ><li class="col01"><input type="checkbox" name="product_id" v-model="sku.selected" @change="update_selected(index)" value="' + abs[i].Id + ',' + abs[i].Id + ',' + abs[i].Name + '" onclick="dod()"></li><li class="col02"><img src=""></li><li class="col03" id="prodtit" >' + abs[i].Name + '</li><li class="col05" id="prodpic">' + abs[i].Price + '元</li><li class="col08">' + abs[i].Count + '</li><li id="pkid" value="' + abs[i].Id + '"><a @click="on_delete(index)" onclick="delpro(' + abs[i].Id + ')">删除</a></li></ul>';
+			alert(abs[i].Id);
+			ul += '<ul class="cart_list_td clearfix" v-for="(sku,index) in cart" id="porcdesc" ><li class="col01"><input type="checkbox" name="product_id" v-model="sku.selected" @change="update_selected(index)" value="' + abs[i].Id + ',' + abs[i].Id + ',' + abs[i].Name + '" onclick="dod()"></li><li class="col02"><img src="">img</li><li class="col03" id="prodtit" >' + abs[i].Name + '</li><li class="col05" id="prodpic">' + abs[i].Price + '元</li><li class="col08">' + abs[i].Count + '</li><li id="pkid" value="' + abs[i].Id + '"><a @click="on_delete(index)" onclick="delpro(' + abs[i].Id + ')">删除</a></li></ul>';
 		}
 
 		ul += '';
