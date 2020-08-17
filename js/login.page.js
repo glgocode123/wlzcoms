c352d9ccccb7ecb190eca7edd5c0361280028a1e
glgocode123 <624601538@qq.com>
2020-08-17 23:14:34 +0800

    Update login.page.js

--- js/login.page.js
+++ js/login.page.js
@@ -6,7 +6,7 @@ $(function () {
 	
 	//cookieMobID需要改为调用cookie中的登录状态，成功就返回手机号
 	var pattern = new RegExp(/^1[3,4,5,7,8][0-9]{9}$/),
-		cookieMobID = 13600000000;
+		cookieMobID = $.cookie("wenlongzhangName");
 	
 	//如果已经登录，跳转到用户中心，并把cookie获得的id传过去
 	
@@ -25,14 +25,16 @@ $(function () {
 //			successMessage = "商品将1～2日内发货，彼时可在会员中心查看快递单号.",
 			error = 0;
 		//如果输入的不是正确的手机号
-        if (!pattern.test($.trim($('.login-form input[name="mob"]').val()))) 
-		{error = 1; $this.find('input[name="mob"]').parent().addClass('invalid'); msg = msg +  '\n - 手机号';}
+        if (!pattern.test($.trim($('.login-form input[name="mob"]').val()))) {
+			error = 1; $this.find('input[name="mob"]').parent().addClass('invalid'); msg = msg +  '\n - 手机号';
+		}
 
         if (error){
         	updateTextPopup('ERROR', msg);
         }else{
 			//直接跳转到i.html
 			cookieMobID = $.trim($('.login-form input[name="mob"]').val());
+			$.cookie("wenlongzhangName", cookieMobID, { expires: 1 });
 			$(location).attr('href', 'i.html?Mob='+cookieMobID);
         }
 	  	return false;
