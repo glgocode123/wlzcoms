013402d5199915095e414462d339556ff773aa8a
glgocode123 <624601538@qq.com>
2020-10-13 22:37:34 +0800

    Update global.diy.js

--- js/global.diy.js
+++ js/global.diy.js
@@ -30,8 +30,7 @@ $(function () {
 			url: 'http://d3j1728523.wicp.vip/',
 			type: 'GET',
 			complete: function (response) {
-				alert(response.status);
-				if (response.status === 200 ) {
+				if (response.status === 200 || response.status === "200") {
 					return true;
 				} else {
 					return false;
