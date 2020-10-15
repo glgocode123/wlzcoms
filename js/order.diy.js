dd140b7b768ce2364084c0ac9f007c06a8301307
glgocode123 <624601538@qq.com>
2020-10-15 16:16:20 +0800

    Update order.diy.js

--- js/order.diy.js
+++ js/order.diy.js
@@ -382,7 +382,7 @@ $(function () {
 			//写入cookie，在立即支付页面再写入数据库
 			$.cookie("wlzOrder" , newCookieData , { expires: 1 });
 			
-			var newData = '{"id": 2,"title": "abc","author": "asdfasdfsdf"}';
+//			var newData = '{"id": 2,"title": "abc","author": "asdfasdfsdf"}';
 			
 			//发送交易请求到w数据库
 			$.ajax({
@@ -390,7 +390,7 @@ $(function () {
 				url: "http://d3j1728523.wicp.vip/posts",
 				async: false,
 				contentType: "application/json",//; charset=utf-8
-				data: newData,
+				data: newJSONData,
 				dataType: "json",
 				success: function (message) {
 					alert(message);
